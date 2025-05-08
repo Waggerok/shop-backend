870e67ab0b3ef020eb4d4503e4cc7c6ebf3f40f0
Waggerok <115732069+Waggerok@users.noreply.github.com>
2025-05-08 18:55:40 +0300

    rename endpoint for feedbacks

--- routes/feedbackRouter.ts
+++ routes/feedbackRouter.ts
@@ -5,20 +5,20 @@ import { Router, Request, Response } from "express";
 const router = Router();
 const controller = new FeedBackController();
 
-router.get('/getFeedBacks', async (req: Request, res : Response) => {
+router.get('/getFeedbacks', async (req: Request, res : Response) => {
     await controller.getFeedBacks(req,res);
 });
 
-router.get('/getFeedBack/:id', async (req: Request, res: Response) => {
+router.get('/getFeedback/:id', async (req: Request, res: Response) => {
     await controller.getFeedBackId(req,res);
 });
 
 
-router.post('/postFeedBack', async (req: Request<IFeedback>, res : Response) => {
+router.post('/postFeedback', async (req: Request<IFeedback>, res : Response) => {
     await controller.postFeedBack(req,res);
 });
 
-router.delete('/deleteFeedBack/:id', async (req : Request, res : Response) => {
+router.delete('/deleteFeedback/:id', async (req : Request, res : Response) => {
     await controller.deleteFeedBack(req,res);
 });
 
